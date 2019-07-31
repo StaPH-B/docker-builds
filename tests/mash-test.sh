d5778a294dbd064c76476fb4f2e3aad9479177d0
kapsakcj <kapsakcj@gmail.com>
2019-07-31 11:33:35 -0400

    added test sub-dir for individual container travis test scripts. These will be called by travis to test each container.

---
+++ tests/mash-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# tests for mash container
+# if anything has an exit code > 0, travis should fail 
+set -e
+docker run mash:2.1 mash
+docker run mash:2.1 mash info /db/RefSeqSketchesDefaults.msh > mashdb.info
+
+echo 'md5sum mashdb.info = '$(md5sum mashdb.info | cut -d ' ' -f 1)
+
+[ "$(md5sum mashdb.info | cut -d ' ' -f 1)" == "a1214deaaa03ef288f4bd063aab8eebd" ]
