6e805e9f687133b6357d624cddacf4b8537a00c7
CJ Jossart <cjjossart@gmail.com>
2022-10-21 15:34:55 -0500

    Added test and test data

--- quicksnp/1.0.0/quicksnp_test.sh
+++ quicksnp/1.0.0/quicksnp_test.sh
@@ -4,4 +4,10 @@
 # exit script with exit code=0 if any test fails
 set -euxo pipefail
 
-QuickSNP.py --help
+QuickSNP.py --dm ./test_data/test_snpdist.tsv --outtree ./test_data/test_1.nwk
+
+if cmp --silent -- ./test_data/test_tree.nwk ./test_data/test_1.nwk; then
+    :
+else
+    echo '### WARNING: Test failed! Check if all files are present and dependencies are the right version. ###'
+fi
