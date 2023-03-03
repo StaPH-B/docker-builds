eb8dba71f42b2d1c880594c3cb906dec6abf6287
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-03 17:22:49 +0000

    Improve awk command readability

--- poppunk/2.6.0/test.sh
+++ poppunk/2.6.0/test.sh
@@ -21,6 +21,6 @@ sed 's/^/prefix_/' rfile.txt > qfile.txt
 poppunk_assign --db database --query qfile.txt --output output --threads $(nproc)
 
 # Compare the database clusters and assigned clusters of the assemblies
-sed 's/^prefix_//' output/output_clusters.csv | awk 'NR==1{print $0;next} {print $0| "sort"}' > assigned.csv
-awk 'NR==1{print $0;next} {print $0| "sort"}' database/database_clusters.csv > database.csv
+sed 's/^prefix_//' output/output_clusters.csv | awk 'NR == 1; NR > 1 { print $0 | "sort" }' > assigned.csv
+awk 'NR == 1; NR > 1 { print $0 | "sort" }' database/database_clusters.csv > database.csv
 cmp assigned.csv database.csv
