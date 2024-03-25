bf942e09ac190a2b311c081f767d98bc73fdb250
Jessica Rowell <JRowell@cdc.gov>
2024-03-25 12:30:23 -0400

    updates to fix the tests

--- polkapox/1.0.0-beta/my_tests.sh
+++ polkapox/1.0.0-beta/my_tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -euo pipefail
+echo "${PWD}"
+for entry in "${PWD}"/*
+do
+  echo "$entry"
+done
+
 gunzip -f LIY15561A65_2022_1744_NE.assembly.gfa.gz
 cat LIY15561A65_2022_1744_NE.assembly.gfa | awk 'BEGIN { FS="\t" } /^S/{ if( length($3) >= 1) print ">Contig"$2"_len"substr($4,6)"_cov"substr($5,6,5)"\n"$3}' | fold > LIY15561A65_2022_1744_NE.contigs.fasta
 
-mpxv-AssemblyGraph_gfaPy.py \
+python mpxv-AssemblyGraph_gfaPy.py \
     -i LIY15561A65_2022_1744_NE.assembly.gfa \
     -r "/scicomp/home-pure/ick4/01.scripts/polkapox/assets/MPXV-UK_P2.noN_39086_40204.fasta" \
     -o .
